5c3f2b0aa71055b943e742bd8b0e124f3921f335
Eric Bishard <e.bishard@gmail.com>
2019-10-04 18:33:44 -0700

    Add simple-flex

--- app/view-components/demoConfigurator.js
+++ app/view-components/demoConfigurator.js
@@ -1,49 +1,30 @@
 import React from "react";
+import { Column, Row } from "simple-flexbox";
 
 const DemoConfigurator = props => {
   const values = props.values;
   return (
     <>
-      <div className="pager-config">
-        <div className="col-md-6">
+      <Row vertical="center">
+        <Column flexGrow={1} horizontal="left">
           <dl>
             <dt>Pager type:</dt>
             <dd>
-              <input
-                type="radio"
-                name="pager"
-                id="numeric"
-                className="k-radio"
-                value="numeric"
-                checked={values.type === "numeric"}
+              <input type="radio" name="pager" id="numeric" className="k-radio" value="numeric" checked={values.type === "numeric"} 
                 onChange={event =>
-                  props.onChange({ value: event.target.value, setting: 'type' })
+                  props.onChange({ value: event.target.value, setting: "type" })
                 }
               />
-              <label
-                style={{ margin: "7px 3em 7px 0px", lineHeight: "1.2" }}
-                className="k-radio-label"
-                htmlFor="numeric"
-              >
+              <label style={{ margin: "7px 3em 7px 0px", lineHeight: "1.2" }} className="k-radio-label" htmlFor="numeric">
                 Numeric&nbsp;
               </label>
 
-              <input
-                type="radio"
-                name="pager"
-                id="input"
-                className="k-radio"
-                value="input"
-                checked={values.type === "input"}
+              <input type="radio" name="pager" id="input" className="k-radio" value="input" checked={values.type === "input"}
                 onChange={event =>
-                  props.onChange({ value: event.target.value, setting: 'type' })
+                  props.onChange({ value: event.target.value, setting: "type" })
                 }
               />
-              <label
-                style={{ margin: "7px 3em 7px 0px", lineHeight: "1.2" }}
-                className="k-radio-label"
-                htmlFor="input"
-              >
+              <label style={{ margin: "7px 3em 7px 0px", lineHeight: "1.2" }} className="k-radio-label" htmlFor="input">
                 Input&nbsp;
               </label>
             </dd>
@@ -56,58 +37,52 @@ const DemoConfigurator = props => {
                 className="k-textbox"
                 type="number"
                 onChange={event =>
-                  props.onChange({ value: event.target.valueAsNumber, setting: 'buttonCount' })
+                  props.onChange({
+                    value: event.target.valueAsNumber,
+                    setting: "buttonCount"
+                  })
                 }
               />
             </dd>
           </dl>
-        </div>
-        <div className="col-md-6 row">
-          <div className="col-md-12">
-            <input
-              className="k-checkbox"
-              checked={values.info}
-              id="showInfo"
-              type="checkbox"
-              onChange={event =>
-                props.onChange({ value: event.target.checked, setting: 'info' })
-              }
-            />
-            <label htmlFor="showInfo" className="k-checkbox-label">
-              Show info
-            </label>
-          </div>
-
-          <div className="col-md-12">
-            <input
-              className="k-checkbox"
-              checked={values.pageSizes}
-              id="pageSize"
-              type="checkbox"
-              onChange={event =>
-                props.onChange({ value: event.target.checked, setting: 'pageSizes' })
-              }
-            />
-            <label htmlFor="pageSize" className="k-checkbox-label">
-              Show page sizes
-            </label>
-          </div>
-          <div className="col-md-12">
-            <input
-              className="k-checkbox"
-              checked={values.previousNext}
-              id="previousNext"
-              type="checkbox"
-              onChange={event =>
-                props.onChange({ value: event.target.checked, setting: 'previousNext' })
-              }
-            />
-            <label htmlFor="previousNext" className="k-checkbox-label">
-              Show previous / next buttons
-            </label>
-          </div>
-        </div>
-      </div>
+        </Column>
+        <Column flexGrow={1} horizontal="left">
+          <input className="k-checkbox" checked={values.info} id="showInfo" type="checkbox"
+            onChange={event =>
+              props.onChange({ value: event.target.checked, setting: "info" })
+            }
+          />
+          <label htmlFor="showInfo" className="k-checkbox-label">
+            Show info
+          </label>
+          <br />
+          <br />
+          <input className="k-checkbox" checked={values.pageSizes} id="pageSize" type="checkbox"
+            onChange={event =>
+              props.onChange({
+                value: event.target.checked,
+                setting: "pageSizes"
+              })
+            }
+          />
+          <label htmlFor="pageSize" className="k-checkbox-label">
+            Show page sizes
+          </label>
+          <br />
+          <br />
+          <input className="k-checkbox" checked={values.previousNext} id="previousNext" type="checkbox"
+            onChange={event =>
+              props.onChange({
+                value: event.target.checked,
+                setting: "previousNext"
+              })
+            }
+          />
+          <label htmlFor="previousNext" className="k-checkbox-label">
+            Show previous / next buttons
+          </label>
+        </Column>
+      </Row>
     </>
   );
 };
