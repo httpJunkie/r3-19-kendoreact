d4171758a34099d4fd56a2f9caf79d5e11545bf7
Eric Bishard <e.bishard@gmail.com>
2019-10-06 15:19:13 -0700

    Be explicit with props as args Reformat and move functions into GridFooterWrap

--- app/view-components/GridFooterWrap.js
+++ app/view-components/GridFooterWrap.js
@@ -2,36 +2,26 @@ import React from "react";
 import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
 import products from "./data/grid-products.json";
 
-const unitPriceCell = props => {
-  const min = products.reduce(
-    (acc, current) => Math.min(acc, current[props.field]),
-    Number.MAX_VALUE
-  );
-  const max = products.reduce(
-    (acc, current) => Math.max(acc, current[props.field]),
-    0
-  );
-  return (
-    <td colSpan={props.colSpan} style={props.style}>
-      min: {min}, max: {max}
-    </td>
-  );
-};
-
-const unitsInStockCell = props => {
-  const total = products.reduce(
-    (acc, current) => acc + current[props.field],
-    0
-  );
-  return (
-    <td colSpan={props.colSpan} style={props.style}>
-      total: {total}
-    </td>
-  );
-};
-
 const GridFooterWrap = () => {
   document.title = `KendoReact Grid ~ Telerik R3 2019 Demo`;
+  const unitPriceCell = (props) => {
+    const min = products.reduce( (acc, current) => Math.min(acc, current[props.field]), Number.MAX_VALUE );
+    const max = products.reduce( (acc, current) => Math.max(acc, current[props.field]), 0 );
+    return (
+      <td colSpan={props.colSpan} style={props.style}>
+        min: {min}, max: {max}
+      </td>
+    );
+  };
+  const unitsInStockCell = (props) => {
+    const total = products.reduce( (acc, current) => acc + current[props.field], 0 );
+    return (
+      <td colSpan={props.colSpan} style={props.style}>
+        total: {total}
+      </td>
+    );
+  }; 
+
   return (
     <Grid style={{ height: "353px" }} data={products}>
       <Column field="ProductID" title="ID" width="70px" />
