fe914ad90c5a4802cb7d234b9c6b9abd4066d755
Eric Bishard <e.bishard@gmail.com>
2019-10-06 15:10:54 -0700

    Fix Grid Height

--- app/view-components/GridFooterWrap.js
+++ app/view-components/GridFooterWrap.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
 import products from "./data/grid-products.json";
 
-console.log("products \n" + products);
-
 const UnitPriceCell = props => {
   const min = products.reduce(
     (acc, current) => Math.min(acc, current[props.field]),
@@ -34,18 +32,12 @@ const UnitsInStockCell = props => {
 
 const GridFooterWrap = () => {
   document.title = `KendoReact Grid ~ Telerik R3 2019 Demo`;
-
-
   return (
-    <Grid style={{ height: "420px" }} data={products}>
+    <Grid style={{ height: "353px" }} data={products}>
       <Column field="ProductID" title="ID" width="70px" />
       <Column field="ProductName" title="Product Name" />
       <Column field="UnitPrice" title="Unit Price" footerCell={UnitPriceCell} />
-      <Column
-        field="UnitsInStock"
-        title="Units In Stock"
-        footerCell={UnitsInStockCell}
-      />
+      <Column field="UnitsInStock" title="Units In Stock" footerCell={UnitsInStockCell} />
       <Column field="Category.CategoryName" title="Category Name" />
     </Grid>
   );
